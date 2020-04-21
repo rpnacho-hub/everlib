febcd9b934feb8a52ea4f8151e830bbb875ee966
Ignacio Rodriguez <irodrpal@everis.com>
2020-04-21 13:43:13 +0200

    Initial project setup

---
+++ EverLib/EverLib/EverLib.h
@@ -0,0 +1,19 @@
+//
+//  EverLib.h
+//  EverLib
+//
+//  Created by Ignacio Rodriguez Palomo on 21/04/2020.
+//  Copyright © 2020 Ignacio Rodriguez Palomo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EverLib.
+FOUNDATION_EXPORT double EverLibVersionNumber;
+
+//! Project version string for EverLib.
+FOUNDATION_EXPORT const unsigned char EverLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EverLib/PublicHeader.h>
+
+
