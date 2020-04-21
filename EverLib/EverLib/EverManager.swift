dee1e58f1947aa65589358369f612065df495f77
Ignacio Rodriguez <irodrpal@everis.com>
2020-04-21 13:51:18 +0200

    Added test cases

---
+++ EverLib/EverLib/EverManager.swift
@@ -0,0 +1,22 @@
+//
+//  EverManager.swift
+//  EverLib
+//
+//  Created by Ignacio Rodriguez Palomo on 21/04/2020.
+//  Copyright © 2020 Ignacio Rodriguez Palomo. All rights reserved.
+//
+
+import Foundation
+public final class EverManager {
+
+    let name = "EverManager"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- EverLib/EverLibTests/EverLibTests.swift
+++ EverLib/EverLibTests/EverLibTests.swift
@@ -11,24 +11,18 @@ import XCTest
 
 class EverLibTests: XCTestCase {
 
+    var manager: EverManager!
+
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        manager = EverManager()
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testAdd() {
+        XCTAssertEqual(manager.add(a: 1, b: 1), 2)
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testSub() {
+        XCTAssertEqual(manager.sub(a: 2, b: 1), 1)
     }
 
 }
